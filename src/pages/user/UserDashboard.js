fa392c3770891068b9415cd445662ecc2a7fad8e
MikeB1124 <balianmichael@gmail.com>
2022-09-01 17:27:55 -0700

    deploy stg front

--- src/pages/user/UserDashboard.js
+++ src/pages/user/UserDashboard.js
@@ -9,7 +9,7 @@ function UserDashboard(){
             </div>
             <div style={{display: 'flex', justifyContent: 'center'}}>
                 <AppIcon appLink={"/apps/daily-specials-board"} title={"Daily Special's Board"} img={"https://images-platform.99static.com//aufFOWBrXYOOZAzgQhR-buEv0KQ=/236x63:895x722/fit-in/590x590/99designs-contests-attachments/53/53255/attachment_53255918"}/>
-                <AppIcon appLink={" "} title={"Customer Kiosk"} img={"https://seeklogo.com/images/K/Kiosk-logo-4948545415-seeklogo.com.gif"}/>
+                <AppIcon appLink={""} title={"Customer Kiosk"} img={"https://seeklogo.com/images/K/Kiosk-logo-4948545415-seeklogo.com.gif"}/>
                 <AppIcon appLink={""} title={"POS Central"} img={"https://dcassetcdn.com/design_img/3987175/838431/27840556/a9qgtnr1h8321t4afggmzbxpk0_image.jpg"}/>
                 <AppIcon appLink={""} title={"Financial Reports"} img={"https://thumbs.dreamstime.com/b/business-report-icon-isolated-white-background-business-report-icon-isolated-white-background-simple-vector-logo-171651784.jpg"}/>
             </div>
