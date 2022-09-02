d6437f90c57447b22cfed265302595c11f315ecf
MikeB1124 <balianmichael@gmail.com>
2022-09-01 18:05:30 -0700

    next

--- server/app.js
+++ server/app.js
@@ -16,7 +16,7 @@ const app = express();
 const server = http.createServer(app)
 const io = new Server(server, {
     cors: {
-        origin: "*",
+        origin: "https://trimana-pos-frontend-stg.herokuapp.com",
         methods: ['GET', 'POST'],
         credentials: true
     }
